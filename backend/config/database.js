86b96bca4afe66357b143a951b42350e6793fd26
vinith <vinithdevadiga7@.com>
2023-04-01 14:45:39 +0530

    op

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,6 +1,5 @@
 const mongoose = require("mongoose");
 mongoose.set("strictQuery", false);
-console.log(process.env.DB_URL)
 const connectDB = () => {
   mongoose
     .connect(process.env.DATABASE_URL, { useNewUrlParser: false })
