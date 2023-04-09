4f2378c8bdd7d2f398849f01d9ed953e46781a05
vinith <vinithdevadiga7@.com>
2023-04-09 19:00:13 +0530

    Updates

---
+++ static/js/425.61602152.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkfrontend=self.webpackChunkfrontend||[]).push([[425],{7425:function(t,h,v){v.d(h,{mYB:function(){return a}});var n=v(9983);function a(t){return(0,n.w_)({tag:"svg",attr:{viewBox:"0 0 24 24"},child:[{tag:"g",attr:{},child:[{tag:"path",attr:{fill:"none",d:"M0 0h24v24H0z"}},{tag:"path",attr:{d:"M22 21H2v-2h1V4a1 1 0 0 1 1-1h14a1 1 0 0 1 1 1v5h2v10h1v2zm-5-2h2v-8h-6v8h2v-6h2v6zm0-10V5H5v14h6V9h6zM7 11h2v2H7v-2zm0 4h2v2H7v-2zm0-8h2v2H7V7z"}}]}]})(t)}}}]);
+//# sourceMappingURL=425.61602152.chunk.js.map
