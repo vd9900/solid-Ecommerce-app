b990a22cf3e719520b8b6582a40ad4f1ed9dd854
vinith <vinithdevadiga7@.com>
2023-04-09 22:15:00 +0530

    ayyo

--- backend/app.js
+++ backend/app.js
@@ -30,7 +30,7 @@ app.use(function (req, res, next) {
 
 // Middleware
 app.use(logger("dev"));
-app.use(cors());
+app.use(cors({ origin: "http://localhost:3000", credentials: true }));
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true, limit: "50mb" }));
 app.use(cookieParser());
