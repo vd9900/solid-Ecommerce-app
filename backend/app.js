bca25ca25c2fafffa13fbec7473291f588616d58
vinith <vinithdevadiga7@.com>
2023-04-11 00:59:33 +0530

    I'm done

--- backend/app.js
+++ backend/app.js
@@ -16,7 +16,7 @@ const app = express();
 
 // Middleware
 app.use(logger("dev"));
-app.use(cors({ origin: "http://localhost:3000", credentials: true }));
+app.use(cors({ origin: ["http://localhost:3000","https://vd9900.github.io/solid-Ecommerce-app"], credentials: true }));
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true, limit: "50mb" }));
 app.use(cookieParser());
